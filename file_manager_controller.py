1f0a1e2e94e20bc4a9afc606c173d6b1f65f7a6b
thalisson <thalissongabriel1999@gmail.com>
2022-03-07 00:24:37 -0300

    file manager

---
+++ file_manager_controller.py
@@ -0,0 +1,48 @@
+import shutil
+import os
+
+
+class FileManagerController:
+    __path = "C:/Users/thali/Downloads"
+    __extension = None
+
+    def __init__(self):
+        for file in self.view_files():
+            self.__create_directory(file)
+            self.__move_file_to_directory(file)
+
+    def view_files(self):
+        return [file for file in os.listdir(self.__path) if os.path.isfile(f"{self.__path}/{file}")]
+
+    def __check_directory(self, file: str):
+        self.__extension = file.split('.')[-1]
+        if os.path.isdir(f"{self.__path}/{self.__extension}") or self.__extension == "ini":
+            return True
+        else:
+            return False
+
+    def __create_directory(self, file: str):
+        if not self.__check_directory(file):
+            os.mkdir(f"{self.__path}/{self.__extension}")
+
+    def __move_file_to_directory(self, file: str):
+        self.__extension = file.split('.')[-1]
+        file_path = f"{self.__path}/{file}"
+        if self.__extension == 'exe':
+            shutil.move(file_path, f'{self.__path}/exe')
+        elif self.__extension == 'torrent':
+            shutil.move(file_path, f'{self.__path}/torrent')
+        elif self.__extension == 'png':
+            shutil.move(file_path, f'{self.__path}/png')
+        elif self.__extension == 'jpg':
+            shutil.move(file_path, f'{self.__path}/jpg')
+        elif self.__extension == 'iso':
+            shutil.move(file_path, f'{self.__path}/iso')
+        elif self.__extension == 'mp4':
+            shutil.move(file_path, f'{self.__path}/mp4')
+        elif self.__extension == 'mkv':
+            shutil.move(file_path, f'{self.__path}/mkv')
+        elif self.__extension == 'pdf':
+            shutil.move(file_path, f'{self.__path}/pdf')
+        elif self.__extension == 'doc':
+            shutil.move(file_path, f'{self.__path}/doc')
---
+++ main.py
@@ -0,0 +1,6 @@
+from file_manager_controller import FileManagerController
+
+
+if __name__ == '__main__':
+    fm = FileManagerController()
+    print(fm.view_files())
